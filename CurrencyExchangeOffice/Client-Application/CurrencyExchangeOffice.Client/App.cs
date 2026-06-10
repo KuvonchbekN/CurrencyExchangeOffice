23f59f1ae35bb01967f456da7223368d44e7bec6
C5400420 <kuvonchbek.normakhmatov@sap.com>
2026-06-10 17:48:55 +0200

    Fix Avalonia client startup

--- CurrencyExchangeOffice/Client-Application/CurrencyExchangeOffice.Client/App.cs
+++ CurrencyExchangeOffice/Client-Application/CurrencyExchangeOffice.Client/App.cs
@@ -1,6 +1,5 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Markup.Xaml.Styling;
 using Avalonia.Themes.Fluent;
 
 namespace CurrencyExchangeOffice.Client;
@@ -10,10 +9,6 @@ public class App : Application
     public override void Initialize()
     {
         Styles.Add(new FluentTheme());
-        Styles.Add(new StyleInclude(new Uri("avares://CurrencyExchangeOffice.Client"))
-        {
-            Source = new Uri("avares://Avalonia.Fonts.Inter/Inter.axaml")
-        });
     }
 
     public override void OnFrameworkInitializationCompleted()
